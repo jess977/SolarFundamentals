1695f183d37ad994e39afc3168cf97e2eac12472
msantos96 <marcosantoniosantos7@hotmail.com>
2020-03-03 20:18:48 +0000

    watera and sanitation

--- GUIApplication/src/main/java/GUIApplication.java
+++ GUIApplication/src/main/java/GUIApplication.java
@@ -1,4 +1,3 @@
-import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 
 import javax.swing.*;
@@ -6,17 +5,85 @@ import java.awt.*;
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
+import javax.swing.event.CellEditorListener;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.JTextField;
+import javax.swing.table.TableCellEditor;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
-import java.beans.ConstructorProperties;
+import java.util.EventObject;
 
 /* TO DO:
-        - Finish water & sanitation
         - Fix resize
  */
+
+class CustomTableCellEditor extends AbstractCellEditor implements TableCellEditor {
+    private TableCellEditor editor;
+
+    @Override
+    public Object getCellEditorValue() {
+        if (editor != null) {
+            return editor.getCellEditorValue();
+        }
+
+        return null;
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        if(column == 0) {
+            if (row == 3) {
+                editor = new DefaultCellEditor(new PreferredSizeComboBox<>(new String[]{
+                        "How far away is the water source in kilometers", "How far away is the water source in walked time"}));
+            }
+            return editor.getTableCellEditorComponent(table, value, isSelected, row, column);
+        }
+
+        switch (row) {
+            case 0: { editor = new DefaultCellEditor(new PreferredSizeComboBox<>(new String[] {"Piped Water", "Water Source"})); break; }
+            case 1: { editor = new DefaultCellEditor(new PreferredSizeComboBox<>(new String[] {"Fetching", "Manual", "Electric"})); break; }
+            case 2: { editor = new DefaultCellEditor(new PreferredSizeComboBox<>(new String[] {"kilometers", "time walked"})); break; }
+            default: editor = new DefaultCellEditor(new JTextField());
+        }
+        return editor.getTableCellEditorComponent(table, value, isSelected, row, column);
+    }
+}
+
+class EnergifyTableModel extends DefaultTableModel {
+    private boolean[][] editable_cells;
+
+    public EnergifyTableModel(Object[][] data, Object[] columnNames) {
+        super(data, columnNames);
+        this.editable_cells = new boolean[data.length][columnNames.length];
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        return this.editable_cells[row][column];
+    }
+
+    public void setColumnEditable(int column, boolean value) {
+        int row = 0;
+        for (boolean[] b : this.editable_cells)
+            setCellEditable(row++, column, value);
+    }
+
+    public void setCellEditable(int row, int column, boolean value) {
+        this.editable_cells[row][column] = value;
+        this.fireTableCellUpdated(row, column);
+    }
+}
+
+class QuestionAnswer extends JPanel {
+    public QuestionAnswer(JLabel label, Component answer) {
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        add(label);
+        add(answer);
+    }
+}
 
 class PreferredSizeComboBox<T> extends JComboBox<T> {
     public PreferredSizeComboBox(T[] items) {
@@ -129,12 +196,14 @@ class EnergifyScrollTablePanel extends JPanel {
 
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.add(tablePanel);
+        scrollPane.setMaximumSize(new Dimension(scrollPane.getMaximumSize().width, 200));
 
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         add(scrollPane);
         add(Box.createRigidArea(new Dimension(0,5)));
         add(buttonsPanel);
     }
+
     public JTable getTable() {return table;}
 }
 
@@ -153,6 +222,7 @@ public class GUIApplication {
         JLabel whatTypeOfEntityLabel = new JLabel("\t\tWhat type of entity?");
         JLabel howFarIsTheBlankFromTheGridLabel = new JLabel();
         JLabel isTheUserCurrentlyLocatedInTheLabel = new JLabel();
+        JLabel doesTheBlankRelyOnPipedWaterOrAnotherWaterSourceLabel = new JLabel(); //belongs to water & sanitation
 
         PreferredSizeComboBox<String> whatTypeOfEntityComboBox = new PreferredSizeComboBox<>(new String[]{"Household", "Community", "Facility"});
         PreferredSizeComboBox<String> howFarIsTheBlankFromTheGridComboBox = new PreferredSizeComboBox<>(new String[]{"Rural/Remote area","Part of urban area"});
@@ -163,8 +233,10 @@ public class GUIApplication {
         isTheUserCurrentlyLocatedInThePanel.setLayout(new GridLayout(2,1,0,0));
 
         whatTypeOfEntityComboBox.addActionListener(actionEvent -> {
-            howFarIsTheBlankFromTheGridLabel.setText(String.format("\t\tHow far is the %s from the grid?", whatTypeOfEntityComboBox.getSelectedItem().toString().toLowerCase()));
-            isTheUserCurrentlyLocatedInTheLabel.setText(String.format("\t\tIs the user currently located in the %s?", whatTypeOfEntityComboBox.getSelectedItem().toString().toLowerCase()));
+            String option = whatTypeOfEntityComboBox.getSelectedItem().toString().toLowerCase();
+            howFarIsTheBlankFromTheGridLabel.setText(String.format("\t\tHow far is the %s from the grid?", option));
+            isTheUserCurrentlyLocatedInTheLabel.setText(String.format("\t\tIs the user currently located in the %s?", option));
+            doesTheBlankRelyOnPipedWaterOrAnotherWaterSourceLabel.setText(String.format("Does the %s rely on piped water or another water source?", option));
         });
 
         whatTypeOfEntityComboBox.setSelectedIndex(0);
@@ -242,7 +314,75 @@ public class GUIApplication {
                         new PreferredSizeComboBox<>(new String[] {"DC", "AC"})));
 
         //----------------- Water & Sanitation -----//
-        JPanel waterAndSanitationPanel = new JPanel();
+        EnergifyTableModel model = new EnergifyTableModel(
+                new Object[][] {
+                        {"Does the %s rely on piped water or another water source?"},
+                        {"How is water obtained from the source ?"},
+                        {"What type of water source?"},
+                        {""},
+                        {"How deep is the water source?"},
+                        {"How much water is used for cooking per household?"},
+                        {"How much water is used in other tasks:"},
+                        {"\t\t\t\tCleaning"},
+                        {"\t\t\t\tWashing clothes"},
+                        {"\t\t\t\tOther"}
+                        }, new String[] {"1","2"});
+
+        JTable waterAndSanitationPanel = new JTable(model);
+        model.setColumnEditable(1, true);
+        model.setCellEditable(3, 0, true);
+        model.setCellEditable(6, 1, false);
+
+        waterAndSanitationPanel.getColumnModel().getColumn(0).setCellEditor(new CustomTableCellEditor());
+        waterAndSanitationPanel.getColumnModel().getColumn(1).setCellEditor(new CustomTableCellEditor());
+
+        /*
+        JPanel waterAndSanitationPanel = new JPanel(new GridLayout(4, 3));
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                doesTheBlankRelyOnPipedWaterOrAnotherWaterSourceLabel,
+                new PreferredSizeComboBox<>(new String[] {"Piped Water", "Water Source"})));
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                new JLabel("How is water obtained from the source ?"),
+                new PreferredSizeComboBox<>(new String[] {"Fetching", "Manual", "Electric"})));
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                new JLabel("What type of water source?"),
+                new PreferredSizeComboBox<>(new String[] {"1", "2"})));
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                new JLabel("How far away is the water source in:"),
+                new PreferredSizeComboBox<>(new String[] {"kilometers", "time walked"})));
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                new JLabel("How deep is the water source?"),
+                new JTextField("In meters", 10)));
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                new JLabel("How much water is used for cooking per household?"),
+                new JTextField("Kilograms/Buckets", 10)));
+
+        waterAndSanitationPanel.add(new JLabel(""),new JPanel());
+
+        waterAndSanitationPanel.add(
+                new JLabel("How much water is used in other tasks:"),
+                new JPanel()
+        );
+
+        waterAndSanitationPanel.add(new JLabel(""),new JPanel());
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                new JLabel("Cleaning"),
+                new JTextField(20)));
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                new JLabel("Washing clothes"),
+                new JTextField(20)));
+
+        waterAndSanitationPanel.add(new QuestionAnswer(
+                new JLabel("Other"),
+                new JTextField(20)));*/
 
         //----------------- Agriculture ------------//
         JPanel agriculturePanel = new JPanel();
